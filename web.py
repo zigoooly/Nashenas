1ad770c496b0080abe96c328f5991b8fd39860fd
zigoooly <mahdimizabi30@gmail.com>
2026-02-02 18:29:58 +0330

    Update web.py

--- web.py
+++ web.py
@@ -5,14 +5,19 @@ from bot import application
 
 app = Flask(__name__)
 
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+# 🔹 فقط یک بار در شروع
+loop.run_until_complete(application.initialize())
+loop.run_until_complete(application.start())
+
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.get_json(force=True)
-
     update = Update.de_json(data, application.bot)
 
-    asyncio.run(application.process_update(update))
-
+    loop.create_task(application.process_update(update))
     return "ok"
 
 @app.route("/")
