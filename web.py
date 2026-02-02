31acedecba0cb96027ff8f1bfc9d6f02816f6255
zigoooly <mahdimizabi30@gmail.com>
2026-02-02 17:47:48 +0330

    Update web.py

--- web.py
+++ web.py
@@ -1,17 +1,21 @@
 from flask import Flask
-import threading
+import asyncio
 from bot import build_app
 
 app = Flask(__name__)
+
 telegram_app = build_app()
 
-def run_bot():
-    telegram_app.run_polling()
+@app.before_first_request
+def start_bot():
+    asyncio.get_event_loop().create_task(telegram_app.initialize())
+    asyncio.get_event_loop().create_task(telegram_app.start())
+    asyncio.get_event_loop().create_task(telegram_app.bot.initialize())
+    asyncio.get_event_loop().create_task(telegram_app.updater.start_polling())
 
 @app.route("/")
 def home():
     return "Bot is running"
 
 if __name__ == "__main__":
-    threading.Thread(target=run_bot).start()
     app.run(host="0.0.0.0", port=8080)
